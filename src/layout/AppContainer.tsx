8e7650cd3106bd9f83397c34389c9d3769052f53
akamu84 <hnviN=Wx_L@1%DT@>
2024-01-01 21:22:11 -0600

    styles for nav

--- src/layout/AppContainer.tsx
+++ src/layout/AppContainer.tsx
@@ -2,22 +2,53 @@ import { Outlet, ScrollRestoration } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import RouterNavLink from "../components/RouterNavLink.tsx";
 import { useOktaAuth } from "@okta/okta-react";
-import { Button } from "@mantine/core";
+import { AppShell, Burger, Button, Stack } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { IconLogout } from "@tabler/icons-react";
 
 const AppContainer = () => {
   const { oktaAuth, authState } = useOktaAuth();
+  const [opened, { toggle }] = useDisclosure();
+
   return (
     <>
-      <div>
-        <RouterNavLink to="/" label="Home" />
-        <RouterNavLink to="/order" label="Orders" />
-        {authState?.isAuthenticated && (
-          <Button onClick={() => oktaAuth.signOut()}>Sign Out</Button>
-        )}
-      </div>
-      <hr />
+      <AppShell
+        header={{ height: 60 }}
+        navbar={{
+          width: 300,
+          breakpoint: "sm",
+          collapsed: { mobile: !opened },
+        }}
+        padding="md"
+      >
+        <AppShell.Header>
+          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+          <div>Logo</div>
+        </AppShell.Header>
+
+        <AppShell.Navbar p="md">
+          <Stack justify="space-between" h="100%">
+            <Stack>
+              <RouterNavLink to="/" label="Home" />
+              <RouterNavLink to="/order" label="Orders" />
+            </Stack>
+            {authState?.isAuthenticated && (
+              <Button
+                variant="transparent"
+                leftSection={<IconLogout />}
+                onClick={() => oktaAuth.signOut()}
+              >
+                Sign Out
+              </Button>
+            )}
+          </Stack>
+        </AppShell.Navbar>
+
+        <AppShell.Main>
+          <Outlet />
+        </AppShell.Main>
+      </AppShell>
       <ScrollRestoration />
-      <Outlet />
       <TanStackRouterDevtools />
     </>
   );
